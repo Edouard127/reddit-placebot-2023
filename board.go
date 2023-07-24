fc7a02c1493486d31771758046cd929dbad0be8f
Alexander Ivanov <saiv46.dev@gmail.com>
2023-07-25 01:03:52 +0800

    Commented out unavailable colors

--- board.go
+++ board.go
@@ -87,35 +87,35 @@ func (b *Board) WaitForData() {
 }
 
 var Colors = map[int]Color{
-	0:  hexToRGB("#6D001A"), // Darkest Red
+	//0:  hexToRGB("#6D001A"), // Darkest Red
 	1:  hexToRGB("#BE0039"), // Dark Red
 	2:  hexToRGB("#FF4500"), // Red
 	3:  hexToRGB("#FFA800"), // Orange
 	4:  hexToRGB("#FFD635"), // Yellow
-	5:  hexToRGB("#FFF8B8"), // Light Yellow
+	//5:  hexToRGB("#FFF8B8"), // Light Yellow
 	6:  hexToRGB("#00A368"), // Dark Green
 	7:  hexToRGB("#00CC78"), // Green
 	8:  hexToRGB("#7EED56"), // Light Green
 	9:  hexToRGB("#00756F"), // Dark Teal
 	10: hexToRGB("#009EAA"), // Teal
-	11: hexToRGB("#00CCC0"), // Light Teal
+	//11: hexToRGB("#00CCC0"), // Light Teal
 	12: hexToRGB("#2450A4"), // Dark Blue
 	13: hexToRGB("#3690EA"), // Blue
 	14: hexToRGB("#51E9F4"), // Light Blue
 	15: hexToRGB("#493AC1"), // Indigo
 	16: hexToRGB("#6A5CFF"), // Periwinkle
-	17: hexToRGB("#94B3FF"), // Lavender
+	//17: hexToRGB("#94B3FF"), // Lavender
 	18: hexToRGB("#811E9F"), // Dark Purple
 	19: hexToRGB("#B44AC0"), // Purple
-	20: hexToRGB("#E4ABFF"), // Light Purple
-	21: hexToRGB("#DE107F"), // Magenta
+	//20: hexToRGB("#E4ABFF"), // Light Purple
+	//21: hexToRGB("#DE107F"), // Magenta
 	22: hexToRGB("#FF3881"), // Pink
 	23: hexToRGB("#FF99AA"), // Light Pink
 	24: hexToRGB("#6D482F"), // Dark Brown
 	25: hexToRGB("#9C6926"), // Brown
 	26: hexToRGB("#FFB470"), // Beige
 	27: hexToRGB("#000000"), // Black
-	28: hexToRGB("#515252"), // Dark Gray
+	//28: hexToRGB("#515252"), // Dark Gray
 	29: hexToRGB("#898D90"), // Gray
 	30: hexToRGB("#D4D7D9"), // Light Gray
 	31: hexToRGB("#FFFFFF"), // White
